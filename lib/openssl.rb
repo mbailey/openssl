e6ab47f56057a365b039935de6b197f9992aa884
Mike Bailey <mike.bailey@rea-group.com>
2015-12-01 14:40:57 +1100

    Initial commit

---
+++ lib/openssl.rb
@@ -0,0 +1,7 @@
+require "openssl/version"
+
+puts "Error! You've somehow installed openssl from rubygems.org"
+puts "Remove openssl from your Gemfile and delete it from your gems"
+
+# module Openssl
+# end
